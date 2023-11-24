044cb8aafc562cda91fb6a63c5eba8af975c945f
GET Academy <terje@getacademy.no>
2023-11-24 11:18:11 +0100

    Initial commit

---
+++ MazeGeneration/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+
+Funksjon generateMaze(width, height):
+    Lag en tom rutenett av størrelse (width, height)
+    Marker alle ruter som ubesøkte
+
+    Velg en tilfeldig startRute
+    Kall generateMazeRecursive(startRute)
+
+    Returner det genererte rutenettet
+
+Funksjon generateMazeRecursive(currentCell):
+    Marker currentCell som besøkt
+
+    Finn alle ubesøkte naboer til currentCell
+    Mens det er ubesøkte naboer:
+        Velg en tilfeldig nabo, nextCell
+        Sett currentCell og nextCell som besøkt
+        Fjern veggen mellom currentCell og nextCell
+
+        Kall generateMazeRecursive(nextCell)
+
+    Returner
+
+
+ 
+ */
